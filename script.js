da48604248a9b120d7e09a60c6a640fa5e0bbaa4
Tiago <tvhollowknight@gmail.com>
2024-10-20 15:03:04 -0300

    Fully implemented and fixed all functionalities in the calculator
    Users can use all functionalities in the calculator

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(num1, num2) {
-	return (parseInt(num1) + parseInt(num2)).toFixed(6);
+	return +(+num1 + +num2).toFixed(6);
 }
 
 function subtract(num1, num2) {
-	return (num1 - num2).toFixed(6);
+	return +(num1 - num2).toFixed(6);
 }
 
 function multiply(num1, num2) {
-	return (num1 * num2).toFixed(6);
+	return +(num1 * num2).toFixed(6);
 }
 
 function divide(num1, num2) {
-	return (num1 / num2).toFixed(6);
+	return +(num1 / num2).toFixed(6);
 }
 
 function operate(operator, num1, num2) {
@@ -28,20 +28,25 @@ function operate(operator, num1, num2) {
 }
 
 // get the references for display and digits
-const digits = document.querySelector(".digits");
+const calculator = document.querySelector(".calculator");
 let display = document.querySelector(".display");
 
-digits.addEventListener("click", (e) => {
+calculator.addEventListener("click", (e) => {
 	// create variables for each element on display
 	let target = e.target;
 	let firstNumOnDisplay = display.textContent.split(" ")[0];
 	let secondNumOnDisplay = display.textContent.split(" ")[2];
 	let operatorOnDisplay = display.textContent.split(" ")[1];
 
+	// pressed somewhere else
+	if (target.id == "") {
+		return;
+	}
+
 	// if a number digit is clicked
 	if (target.id >= 0 && target.id <= 9) {
 		// don't allow for repeatedly pressing the 0 button if not inside a number
-		if (firstNumOnDisplay == "0") {
+		if (firstNumOnDisplay == "0" && operatorOnDisplay == undefined) {
 			display.textContent = display.textContent.replace(
 				firstNumOnDisplay,
 				target.id
@@ -65,6 +70,10 @@ digits.addEventListener("click", (e) => {
 		// if there is a full operation on display, finish this one and start the other
 		else if (["x", "÷", "+", "-"].includes(operatorOnDisplay)) {
 			if (secondNumOnDisplay == "") {
+				display.textContent = display.textContent.replace(
+					operatorOnDisplay,
+					target.id
+				);
 				return;
 			}
 			display.textContent = operate(
@@ -74,26 +83,28 @@ digits.addEventListener("click", (e) => {
 			);
 			display.textContent += " " + target.id + " ";
 		}
-		// if the user presses an operation before finishing one
-		else {
-			display.textContent = display.textContent.replace(
-				operatorOnDisplay,
-				target.id
-			);
-		}
 	}
 	// if the fraction digit is clicked
 	else if (target.id == ".") {
 		// if both the elements on display aren't fractional add the .
 		if (
-			(!firstNumOnDisplay.includes(".") && !secondNumOnDisplay.includes(".")) ||
-			!(display.textContent.at(-1) == " ")
+			!firstNumOnDisplay.includes(".") &&
+			display.textContent.at(-1) != " " &&
+			firstNumOnDisplay != ""
 		) {
-			display.textContent += ".";
+			if (
+				secondNumOnDisplay != undefined &&
+				!secondNumOnDisplay.includes(".")
+			) {
+				display.textContent += ".";
+			} else if (secondNumOnDisplay == undefined) {
+				display.textContent += ".";
+			}
 		}
 		// if the second element on display isn't fractional and the first is, add the .
 		else if (
 			!secondNumOnDisplay.includes(".") &&
+			secondNumOnDisplay != "" &&
 			firstNumOnDisplay.includes(".")
 		) {
 			display.textContent += ".";
@@ -109,5 +120,49 @@ digits.addEventListener("click", (e) => {
 				secondNumOnDisplay
 			);
 		}
+	}
+	// pressed the C button
+	else if (target.id == "C") {
+		display.textContent = "0";
+		firstNumOnDisplay = "0";
+		operatorOnDisplay = undefined;
+		secondNumOnDisplay = undefined;
+	}
+	// pressed the CE button
+	else if (target.id == "CE") {
+		if (secondNumOnDisplay == undefined || secondNumOnDisplay == "") {
+			display.textContent = "0";
+			firstNumOnDisplay = "0";
+			operatorOnDisplay = "";
+			secondNumOnDisplay = "";
+		} else {
+			display.textContent =
+				firstNumOnDisplay + " " + operatorOnDisplay + " " + "";
+			secondNumOnDisplay = undefined;
+		}
+	}
+});
+
+calculator.addEventListener("mouseover", (e) => {
+	let target = e.target;
+
+	if (
+		((target.id >= 0 && target.id <= 9) ||
+			["+", "-", "x", "÷", "=", ".", "CE", "C"].includes(target.id)) &&
+		target.id != ""
+	) {
+		target.style.opacity = "0.6";
+	}
+});
+
+calculator.addEventListener("mouseout", (e) => {
+	let target = e.target;
+
+	if (
+		((target.id >= 0 && target.id <= 9) ||
+			["+", "-", "x", "÷", "=", ".", "CE", "C"].includes(target.id)) &&
+		target.id != ""
+	) {
+		target.style.opacity = "1";
 	}
 });
